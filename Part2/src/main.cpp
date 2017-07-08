5b068c81e58a5c4cfba693a419e679927a066caf
Administrator <admin@example.com>
2017-07-08 15:26:09 -0400

    Fixed filename extraction

--- Part2/src/main.cpp
+++ Part2/src/main.cpp
@@ -13,21 +13,21 @@ namespace fs = boost::filesystem;
 
 void CreateDependencyGraph(const std::string& rootDir)
 {
-    const std::regex includeRx{ "#include (<|\")[a-zA-Z0-9]+\\.h(>|\")" };
+    const std::regex includeRx{ "#include (<|\")([a-zA-Z0-9]+\\.h)(>|\")" };
 
     for(const auto& dirEntry : fs::recursive_directory_iterator(rootDir))
     {
         if(fs::is_regular_file(dirEntry))
         {
-            //const std::string fileContent{ std::istreambuf_iterator<char>(std::ifstream(fs::path(dirEntry).string())), 
-            //                               std::istreambuf_iterator<char>() };
+            std::ifstream fileStream(fs::path(dirEntry).string());
+            const std::string fileContent{ std::istreambuf_iterator<char>{fileStream}, 
+                                           std::istreambuf_iterator<char>{} };
             
-            const std::string fileContent{ fs::path(dirEntry).string() }; 
             auto rxIt = std::sregex_iterator(fileContent.begin(), fileContent.end(), includeRx);
             auto rxEnd = std::sregex_iterator();
             for(; rxIt != rxEnd; ++rxIt)
             {
-                std::cout << rxIt->str();
+                std::cout << rxIt->str(2) << "\n";
             }            
         }
     }
